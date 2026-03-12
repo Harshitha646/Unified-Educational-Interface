f2bc0f3a8ebc017c9cb5218e1270ea75a64a328a
Harshitha646 <harshithasenthil99@gmail.com>
2026-03-12 19:29:47 +0530

    Initial commit - Unified Educational Interface

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("UEI Backend Running 🚀");
+});
+
+app.post("/login",(req,res)=>{
+
+const {username,password} = req.body
+
+if(username === "student" && password === "1234"){
+
+res.json({message:"Login Successful"})
+
+}else{
+
+res.json({message:"Invalid Login"})
+
+}
+
+})
+
+app.listen(5000,()=>{
+console.log("Server running on http://localhost:5000")
+})
---
+++ js/script.js
@@ -0,0 +1,69 @@
+function loginCheck(){
+
+const username = document.querySelector('input[type="text"]').value
+const password = document.querySelector('input[type="password"]').value
+
+fetch("http://localhost:5000/login",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+username:username,
+password:password
+})
+})
+.then(res => res.json())
+.then(data => {
+
+if(data.message === "Login Successful"){
+
+localStorage.setItem("studentName", username)
+
+window.location.href = "student.html"
+
+}else{
+
+alert("Invalid Login")
+
+}
+
+})
+
+return false
+
+}
+
+}
+
+}
+function logout(){
+localStorage.removeItem("studentName");
+window.location.href = "login.html";
+}
+
+function submitFeedback(event){
+    event.preventDefault();
+    alert("Thank you! Your feedback has been submitted.");
+    event.target.reset();
+}
+
+
+/* Show student name in navbar */
+
+const studentName = localStorage.getItem("studentName");
+
+if(studentName){
+document.getElementById("studentName").innerText = "👤 " + studentName;
+}
+
+const name = localStorage.getItem("studentName");
+
+if(name){
+const welcome = document.getElementById("welcomeUser");
+
+if(welcome){
+welcome.innerText = "Welcome " + name + " 👋";
+}
+}
+
